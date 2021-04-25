fcaadb2a8c9124d954e3e5b8ee15d51997ef9a91
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-04-24 22:11:11 -0500

    JCMA Creación de la clase Operaciones y mensaje de saludo

---
+++ OperacionesMatematicas/src/main/java/com/repaso/clases/Operaciones.java
@@ -0,0 +1,12 @@
+package com.repaso.clases;
+
+public class Operaciones {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("*** Esta es una p´ractica de GIT ***");
+		System.out.println("Programa que realiza las operaciones básicas");
+		
+	}
+
+}
