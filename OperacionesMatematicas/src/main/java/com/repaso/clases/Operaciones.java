a27d92c104d734b2b2179bdab65b3b73bf589a90
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-04-24 22:14:28 -0500

    JCMA Creación de la operación de Resta

--- OperacionesMatematicas/src/main/java/com/repaso/clases/Operaciones.java
+++ OperacionesMatematicas/src/main/java/com/repaso/clases/Operaciones.java
@@ -7,6 +7,12 @@ public class Operaciones {
 		System.out.println("*** Esta es una p´ractica de GIT ***");
 		System.out.println("Programa que realiza las operaciones básicas");
 		
+		System.out.println("Operación 1: Resta");
+		int a = 12;
+		int b = 32;
+		int c = a - b;
+		System.out.println("El resultado de la resta es: " + c);
+		
 	}
 
 }
